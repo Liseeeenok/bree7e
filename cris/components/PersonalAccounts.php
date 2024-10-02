c5c51936572eb85e58a0137552013f6ea58b604c
Liseeeenok <vlad.vedernikov.95@mail.ru>
2024-10-02 04:36:01 +0000

    Fix zav_lab

--- cris/components/PersonalAccounts.php
+++ cris/components/PersonalAccounts.php
@@ -620,7 +620,7 @@ class PersonalAccounts extends ComponentBase
         $NIR = post('NIR') ?? "";
         $information = post('information') ?? "";
         $user_id = post('user_id');
-        $id_zav_lab_otdel = post('id_zav_lab_otdel') ?? null;
+        $id_zav_lab_otdel = post('id_zav_lab_otdel') ?: null;
         
         $this->page['id_author_reference'] = AuthorReference::create([
             'full_name_publication' => $full_name_publication,
